c5f1ecd11eceed0c88e8fa0959b95242e65f5ad2
amandlek <ajay.mandlekar@gmail.com>
2021-07-16 01:30:31 -0700

    some fixes for pip release

--- setup.py
+++ setup.py
@@ -5,24 +5,38 @@ import os
 import sys
 import shutil
 import subprocess
-from setuptools import setup, find_packages
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
 
 
-def build_cmake():
-    try:
-        out = subprocess.check_output(['cmake', '--version'])
-    except OSError:
-        raise RuntimeError("CMake must be installed.")
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
-    # make build directory
-    here = os.path.abspath(os.path.dirname(__file__))
-    build_dir = os.path.join(here, "egl_probe", "build")
-    if os.path.exists(build_dir):
-        shutil.rmtree(build_dir)
-    os.mkdir(build_dir)
 
-    # build using cmake
-    subprocess.check_call("cd egl_probe/build; cmake ..; make -j", shell=True)
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed.")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        # make build directory
+        build_dir = os.path.join(extdir, "egl_probe", "build")
+        if os.path.exists(build_dir):
+            shutil.rmtree(build_dir)
+        os.mkdir(build_dir)
+
+        # build using cmake
+        subprocess.check_call("cmake ..; make -j", cwd=build_dir, shell=True)
+
 
 if sys.version_info.major == 3:
     with open("README.md", "r", encoding="utf-8") as fh:
@@ -32,15 +46,17 @@ else:
     with open("README.md", "r") as fh:
         long_description = fh.read()
 
-build_cmake()
+
 setup(
     name='egl_probe',
-    version='1.0.0',
+    version='1.0.1',
     author='Stanford University',
     url='https://github.com/StanfordVL/egl_probe',
     zip_safe=False,
     packages=find_packages(),
     install_requires=[],
+    ext_modules=[CMakeExtension('EGLProbe', sourcedir='egl_probe')],
+    cmdclass=dict(build_ext=CMakeBuild),
     tests_require=[],
     include_package_data=True,
     long_description=long_description,
